ee8ee9f623913dc52706b9a3b0b9af481d450716
subz3r0beatz <flavio.dumas@outlook.es>
2025-12-07 05:25:02 +0100

    win to unix

---
+++ check_network.sh
@@ -0,0 +1,19 @@
+echo -e "${BLUE}[2/11] Setting Up Network Connection...${NC}"
+
+echo -e "${YELLOW}Checking Network...${NC}"
+for i in {1..5}; do
+  if ping -c 1 archlinux.org &> /dev/null; then
+    echo -e "${GREEN}Internet Connected!${NC}"
+    break
+  else
+    echo -e "${YELLOW}Waiting for internet...${NC}"
+    sleep 2
+  fi
+done
+
+if ! ping -c 1 archlinux.org &> /dev/null; then
+  echo -e "${RED}No Internet Connection!${NC}\n${YELLOW}(Run 'iwctl' then restart script)${NC}"
+  exit 1
+fi
+
+echo -e "${GREEN}Network Setup Finished!${NC}"
