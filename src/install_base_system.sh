f0cfccd4d49136cef47c485b9a2433c6f41a730d
Flavio Dumas Ruiz <81779397+subz3r0beatz@users.noreply.github.com>
2025-12-09 04:09:11 +0100

    dumb shellcheck telling me to quote bs

--- src/install_base_system.sh
+++ src/install_base_system.sh
@@ -10,7 +10,7 @@ echo -e "${BLUE}[6/10] Installing Base System...${NC}"
 
 DRIVERS="mesa mesa-utils intel-ucode vulkan-intel libva-intel-driver vulkan-radeon xf86-video-amdgpu"
 
-pacstrap /mnt base linux linux-headers linux-firmware lvm2 btrfs-progs neovim networkmanager grub efibootmgr git base-devel archlinux-keyring go "$DRIVERS"
+pacstrap /mnt base linux linux-headers linux-firmware lvm2 btrfs-progs neovim networkmanager grub efibootmgr git base-devel archlinux-keyring go $DRIVERS
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
